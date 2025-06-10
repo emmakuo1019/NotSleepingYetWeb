32e78ec04522a1d58287457cc4bdfa528c18d89a
emmakuo1019 <154528242+emmakuo1019@users.noreply.github.com>
2025-06-10 18:45:19 +0800

    最後掙扎了

--- tias.js
+++ tias.js
@@ -43,4 +43,22 @@ $(document).ready(function () {
             {behavior: "smooth"}
         )
     });
+    //進入視窗效果
+    /*const elements = document.querySelectorAll(".title");
+
+    function observeHandler(entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+            } else {
+                entry.target.classList.remove("visible"); // 離開視窗就移除 class，讓它可以再次觸發
+            }
+        });
+    }
+
+    const observer = new IntersectionObserver(observeHandler);
+
+    elements.forEach(element => {
+        observer.observe(element);
+    });*/
 })
